13563fbdf1acb2670b9f275956082b6cc17e4392
Denis Vybornyj <denis.vybornyj@gmail.com>
2021-06-25 14:26:16 +0300

    feat: added ether mainnet

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -99,13 +99,13 @@ export const WETH = {
   //   '',
   //   ''
   // ),
-  // [ChainId.ETHER_MAINNET]: new Token(
-  //   ChainId.ETHER_MAINNET,
-  //   '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-  //   18,
-  //   'WETH',
-  //   'Wrapped Ether'
-  // ),
+  [ChainId.ETHER_MAINNET]: new Token(
+    ChainId.ETHER_MAINNET,
+    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
   // [ChainId.ETHER_TESTNET]: new Token(
   //   ChainId.ETHER_TESTNET,
   //   '',
