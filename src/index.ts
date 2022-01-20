7d1f0a562c66d337c1b8048c181a1bb6dab49c6b
sieugene <sieugene@mail.ru>
2022-01-20 12:12:35 +0300

    feat : exchange config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import JSBI from 'jsbi'
 export { JSBI }
 
-export {
-  BigintIsh,
-  ChainId,
-  TradeType,
-  Rounding,
-  FACTORY_ADDRESS,
-  INIT_CODE_HASH,
-  ROUTER_ADDRESS,
-  MINIMUM_LIQUIDITY,
-  SIDE_ROUTER_ADDRESS
-} from './constants'
+export { BigintIsh, ChainId, TradeType, Rounding, EXCHANGE_CONFIG, MINIMUM_LIQUIDITY } from './constants'
 
 export * from './errors'
 export * from './entities'
