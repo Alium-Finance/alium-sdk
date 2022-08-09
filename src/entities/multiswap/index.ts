8ee155db97bfe64f9b1d80256329761d6f293570
sieugene <sieugene@mail.ru>
2022-08-09 17:21:32 +0300

    feat : multiswap foreign calc

---
+++ src/entities/multiswap/index.ts
@@ -0,0 +1,54 @@
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { ALM_TOKENS, ChainId, Swap, SwapArgs, WETH, wrappedCurrency } from '../..'
+
+export class Multiswap {
+  constructor(private readonly foreignProvider: JsonRpcProvider, private readonly foreignChainId: ChainId) {}
+
+  public async rateForeignChain(args: Omit<SwapArgs, 'currencyA' | 'comparator'>) {
+    const { currencyB } = args
+
+    const swap = new Swap(this.foreignProvider, this.foreignChainId)
+    const STABLE_FOREIGN = this.getStable()
+    const ALM_FOREIGN = ALM_TOKENS[this.foreignChainId]
+    const receiveTokenIsCore = currencyB && wrappedCurrency(currencyB, this.foreignChainId)?.equals?.(STABLE_FOREIGN)
+
+    if (!receiveTokenIsCore) {
+      // Calc receive stable token of alm token (only alium liquidity)
+      const tradeToStable = await swap.swapExactIn({
+        ...args,
+        currencyA: STABLE_FOREIGN,
+        currencyB: ALM_FOREIGN,
+        comparator: 'onlyA'
+      })
+      // Calc receive token of core token (hybrid strategy)
+      const tradeToErc20 = await swap.swapExactIn({
+        ...args,
+        currencyA: currencyB,
+        currencyB: STABLE_FOREIGN,
+        amount: tradeToStable?.trade?.outputAmount?.toSignificant(6) || '0',
+        comparator: 'hybrid'
+      })
+
+      return {
+        tradeToStable,
+        tradeToErc20
+      }
+    } else {
+      const tradeToErc20 = await swap.swapExactIn({
+        ...args,
+        currencyA: ALM_FOREIGN,
+        comparator: 'hybrid'
+      })
+
+      return {
+        tradeToStable: null,
+        tradeToErc20
+      }
+    }
+  }
+
+  // take from tokens list later
+  private getStable() {
+    return WETH[this.foreignChainId]
+  }
+}
