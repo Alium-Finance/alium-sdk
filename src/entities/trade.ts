195a8974182d3acba4aed21ea582e507f4ab7396
sieugene <sieugene@mail.ru>
2022-12-13 20:07:44 +0900

    fix : comparator

--- src/entities/trade.ts
+++ src/entities/trade.ts
@@ -73,7 +73,8 @@ export function hybridComparator(tradeA: Trade | null, tradeB: Trade | null) {
   if (tradeA && tradeB) {
     const { outputAmount: receiveCountA, priceImpact: priceImpactA } = tradeA
     const { outputAmount: receiveCountB, priceImpact: priceImpactB } = tradeB
-    const priceImpactTradeAFailure = !priceImpactA?.lessThan(BLOCKED_PRICE_IMPACT_NON_EXPERT)
+    const priceImpAHigh = !priceImpactA?.lessThan(BLOCKED_PRICE_IMPACT_NON_EXPERT)
+    const priceImpBHigh = !priceImpactB?.lessThan(BLOCKED_PRICE_IMPACT_NON_EXPERT)
     const isTradeAProfit = receiveCountA.greaterThan(receiveCountB) || receiveCountA.equalTo(receiveCountB)
     console.info(
       `COMPARATOR :: can be receive: tradeA - ${receiveCountA?.toSignificant(
@@ -82,7 +83,11 @@ export function hybridComparator(tradeA: Trade | null, tradeB: Trade | null) {
         4
       )}, priceImpactB - ${priceImpactB.toSignificant(4)}`
     )
-    return isTradeAProfit && !priceImpactTradeAFailure ? tradeA : tradeB
+    if (isTradeAProfit) {
+      return priceImpAHigh ? tradeB : tradeA
+    } else {
+      return priceImpBHigh ? tradeA : tradeB
+    }
   }
   console.info(`COMPARATOR ::  chose trade: ${(!!tradeA && 'tradeA') || (!!tradeB && 'tradeB') || 'null'}`)
   return tradeA || tradeB
